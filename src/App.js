1c071cc3fb6f549159cfb850f4b780ae4aaa62b7
mahmud <you@example.com>
2020-06-13 02:34:01 +0600

    integrated api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import WeatherCard from './components/WeatherCard/Component'
 
 import './App.css';
 
 function App() {
 
+  const[query, setQuery] = useState('london, gb')
+  const[weather, setWeather] = useState({
+    temp: null,
+    city: null,
+    condition: null,
+    country: null,
+  })
+ 
+
+  const data = async(q) => {
+    const apiRes = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=70874bf3c19c9eb4e8a7483d70f2d1e9`
+    );
+    const resJSON = await apiRes.json();
+    return resJSON
+  }
+  const handleSearch= e =>{
+    e.preventDefault()
+    data(query).then(res => {
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.sys.country,
+      });
+    })
+  }
+
+    useEffect(() => {
+      data(query).then((res) => {
+        setWeather({
+          temp: res.main.temp,
+          city: res.name,
+          condition: res.weather[0].main,
+          country: res.sys.country,
+        });
+      });
+    }, []);
+
   return (
     <div className="App">
-      <WeatherCard temp={-20} condition="Clear" city="New York" country="USA" />
-      <WeatherCard temp={15} condition="Cloudy" city="Sydney" country="AU" />
-      <WeatherCard temp={40} condition="Tornado" city="London" country="UK" />
+      <WeatherCard temp={weather.temp} condition={weather.condition} city={weather.city} country={weather.country} />
+      <form action="">
+        <input value={query} onChange={e => setQuery(e.target.value)}/>
+        <button onClick={e => handleSearch(e)}>Search</button>
+      </form>
     </div>
   );
 }
